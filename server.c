d5c8c1645716e54e2b52fb8c88c67c780d0be5d6
C4llSqin <83995467+C4llSqin@users.noreply.github.com>
2026-04-27 10:33:41 -0600

    switch from tabs to spaces

--- server.c
+++ server.c
@@ -7,56 +7,56 @@
 #include <signal.h>
 
 struct message {
-	char source[50];
-	char target[50]; 
-	char msg[200]; // message body
+    char source[50];
+    char target[50]; 
+    char msg[200]; // message body
 };
 
 void terminate(int sig) {
-	printf("Exiting....\n");
-	fflush(stdout);
-	exit(0);
+    printf("Exiting....\n");
+    fflush(stdout);
+    exit(0);
 }
 
 int main() {
-	int server;
-	int target;
-	int dummyfd;
-	struct message req;
-	signal(SIGPIPE,SIG_IGN);
-	signal(SIGINT,terminate);
-	server = open("serverFIFO",O_RDONLY);
-	dummyfd = open("serverFIFO",O_WRONLY);
+    int server;
+    int target;
+    int dummyfd;
+    struct message req;
+    signal(SIGPIPE,SIG_IGN);
+    signal(SIGINT,terminate);
+    server = open("serverFIFO",O_RDONLY);
+    dummyfd = open("serverFIFO",O_WRONLY);
 
-	while (1) {
-		// TODO:
-		// read requests from serverFIFO
-		read(server, &req, sizeof(struct message));
-
-
-
-
-
-
-		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-
-		// TODO:
-		// open target FIFO and write the whole message struct to the target FIFO
-		// close target FIFO after writing the message
-		target = open(req.target, O_WRONLY);
-		write(target, &req, sizeof(struct message));
-		close(target);
+    while (1) {
+        // TODO:
+        // read requests from serverFIFO
+        read(server, &req, sizeof(struct message));
 
 
 
 
 
 
+        printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
+
+        // TODO:
+        // open target FIFO and write the whole message struct to the target FIFO
+        // close target FIFO after writing the message
+        target = open(req.target, O_WRONLY);
+        write(target, &req, sizeof(struct message));
+        close(target);
 
 
-	}
-	close(server);
-	close(dummyfd);
-	return 0;
+
+
+
+
+
+
+    }
+    close(server);
+    close(dummyfd);
+    return 0;
 }
 
